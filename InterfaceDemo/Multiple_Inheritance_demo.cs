46723b0ac21db0846dcbbf5e1c4c6222512145de
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-12-19 13:19:03 +0530

    INTERFACE EXAMPLES

---
+++ InterfaceDemo/Multiple_Inheritance_demo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+
+    public interface IPrinterTasks
+    {
+        void Print(string PrintContent);
+        void Scan(string ScanContent);
+    }
+    interface IFaxTasks
+    {
+        void Fax(string content);
+    }
+    interface IPrintDuplexTasks
+    {
+        void PrintDuplex(string content);
+    }
+    //Implementation of interface methods using inheritance 
+    public class HPLaserJetPrinter : IPrinterTasks, IFaxTasks, IPrintDuplexTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine(PrintContent);
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine(ScanContent);
+        }
+        public void Fax(string FaxContent)
+        {
+            Console.WriteLine(FaxContent);
+        }
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            Console.WriteLine(PrintDuplexContent);
+        }
+    }
+    class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine(PrintContent);
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine(ScanContent);
+        }
+    }
+    public class Multiple_Inheritance_demo
+    {
+        static void Main(string[] args)
+        {
+            HPLaserJetPrinter hPLaserJetPrinter = new HPLaserJetPrinter();
+            hPLaserJetPrinter.Scan("Scan Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.Print("Print Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.Fax("Fax Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.PrintDuplex("Print Duplex Services by HPLaserJetPrinter");
+            LiquidInkjetPrinter liquidInkjetPrinter = new LiquidInkjetPrinter();
+            liquidInkjetPrinter.Scan("Scan Services by LiquidInkjetPrinter");
+            liquidInkjetPrinter.Print("Print Services by LiquidInkjetPrinter");
+            //Fax and PrintDuplex are not available in LiquidInkjetPrinter
+            //liquidInkjetPrinter.Fax("Fax Services");
+            //liquidInkjetPrinter.PrintDuplex("Print Duplex Services");
+            Console.Read();
+        }
+    }
+}
---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+
+    interface ITestInterface
+    {
+        void Sum(int num1, int num2);
+        void mul(int num1, int num2);
+    }
+
+    public class ImplementationClass : ITestInterface
+    {
+        void ITestInterface.mul(int num1, int num2)
+        {
+            Console.WriteLine($"Product of {num1} and {num2} is {num1 * num2}");
+        }
+
+        public void Sum(int num1, int num2)
+        {
+            Console.WriteLine($"Sum of {num1} and {num2} is {num1 + num2}");
+
+        }
+    }
+    public  class Program
+    {
+        static void Main4(string[] args)
+        {
+            ImplementationClass obj1= new ImplementationClass();
+            //Using obj1 we can call the Add method directly because
+            //It is implemented using public access specifier
+            obj1.Sum(1, 2);
+            //We need to typecast obj1 to ITestInterface1 to call the Sub
+            //method because Sub method is implemented using Interface name
+            ((ITestInterface)obj1).mul(1, 2);
+            ITestInterface obj2= new ImplementationClass();
+            //We can call the method directly using the interface reference
+            //Typecasting is not required in this case
+            obj2.Sum(1, 3);
+            obj2.mul(1, 3);
+        }
+    }
+}
